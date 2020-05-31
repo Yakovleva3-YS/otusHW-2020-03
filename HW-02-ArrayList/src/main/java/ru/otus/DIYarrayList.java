d9b2ebeaa4c92c8cc834b175967d60f35be07ea5
yakovleva3-ys <yanasergeevnaya@yandex.ru>
2020-05-31 21:49:58 +0300

    HW-02 DIYarrayList

--- HW-02-ArrayList/src/main/java/ru/otus/DIYarrayList.java
+++ HW-02-ArrayList/src/main/java/ru/otus/DIYarrayList.java
@@ -47,167 +47,100 @@ public class DIYarrayList<T> implements List<T> {
 
     @Override
     public ListIterator<T> listIterator() {
-        return new ListItr();
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     public void sort(Comparator<? super T> c){
-        Arrays.sort((T[])elements, c);
+        Arrays.sort((T[])elements, 0, size, c);
     }
 
     @Override
     public ListIterator<T> listIterator(int index) {
-        return null;
+        throw new UnsupportedOperationException("The method is not implemented");
     }
-
-//---------------------------------------------------------------------------------------------------
-
-    private class Itr implements Iterator<T>{
-
-        @Override
-        public boolean hasNext() {
-            return false;
-        }
-
-        @Override
-        public T next() {
-            return null;
-        }
-    }
-
-//---------------------------------------------------------------------------------------------------
-
-    private class ListItr extends Itr implements ListIterator<T>{
-
-        @Override
-        public boolean hasNext() {
-            return false;
-        }
-
-        @Override
-        public T next() {
-            return null;
-        }
-
-        @Override
-        public boolean hasPrevious() {
-            return false;
-        }
-
-        @Override
-        public T previous() {
-            return null;
-        }
-
-        @Override
-        public int nextIndex() {
-            return 0;
-        }
-
-        @Override
-        public int previousIndex() {
-            return 0;
-        }
-
-        @Override
-        public void remove() {
-
-        }
-
-        @Override
-        public void set(T e) {
-
-        }
-
-        @Override
-        public void add(T e) {
-
-        }
-    }
-
-//------------------------------------------------------------------------------------------------------------------
 
     @Override
     public boolean isEmpty() {
-        return false;
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public <T1> T1[] toArray(T1[] a) {
-        return null;
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public boolean addAll(Collection<? extends T> c) {
-        return false;
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends T> c) {
-        return false;
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public void clear() {
-
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public List<T> subList(int fromIndex, int toIndex) {
-        return null;
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 
     @Override
     public void add(int index, T element) {
-
+        throw new UnsupportedOperationException("The method is not implemented");
     }
 }
--- HW-02-ArrayList/src/main/java/ru/otus/Main.java
+++ HW-02-ArrayList/src/main/java/ru/otus/Main.java
@@ -5,91 +5,57 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-//        List<Integer> myList = new MyArrayList<>();
-//        Collections.addAll(myList, 1, 2, 3, 4, 5);
-//        for (Integer value:myList) {
-//            System.out.println(value);
-//        }
-//        test();
-        testArray();
-//        testCopy();
-//        testSort();
+        testAddAll();
+        testCopy();
+        testSort();
     }
 
-    public static void test() {
-        List<String> dfs = new DIYarrayList<>();
-        dfs.add("First");
-        dfs.add("Second");
-        dfs.set(1, "Third");
-        Collections.addAll(dfs, "Four", "Five", "Six", "Seven", "8", "9", "10", "12");
-        dfs.add("11");
-        for(int i=0; i<dfs.size(); i++) {
-            System.out.println(dfs.get(i));
-        }
-    }
-
-    public static void testArray() {
-        List<String> dfs = new ArrayList<>();
-        dfs.add("First");
-        dfs.add("Second");
-        dfs.set(1, "Third");
-        Collections.addAll(dfs, "Four", "Five", "Six", "Seven", "8", "9", "10", "12");
-
-        for (String value: dfs) {
-            System.out.println(value);
-        }
-
-        System.out.println("------------");
-
-        Collections.sort(dfs);
-        dfs.stream().forEach(System.out::println);
-//        ListIterator<String> listIterator = dfs.listIterator();
-//        for(int i=0; i<2; i++) {
-//            System.out.println(listIterator.next());
-//            System.out.println(listIterator.nextIndex());
-//        }
-
-    }
-
-    public static void testCopy() {
+    public static void testAddAll() {
+        System.out.println("----Test addAll----");
         List<String> array = new DIYarrayList<>();
-        array.add("First");
-        array.add("Second");
-
-        List<String> array2 = new DIYarrayList<>();
-        array2.add("3");
-        array2.add("4");
-        ListIterator<String> listIterator = array.listIterator();
-        Collections.copy(array, array2);
+        array.add("1");
+        array.add("2");
+        array.set(1, "3");
+        Collections.addAll(array,
+                "Four", "Five", "Six", "Seven", "8", "9", "10", "12", "13",
+                "14", "15", "16", "17", "18", "19", "20", "21");
+        array.add("11");
         for(int i=0; i<array.size(); i++) {
             System.out.println(array.get(i));
         }
+    }
 
-        for(int i=0; i<array2.size(); i++) {
-            System.out.println(array2.get(i));
+    public static void testCopy() {
+        System.out.println("----Test copy----");
+        List<Integer> array = new ArrayList<>();
+        for (int i = 0; i<22; i++) {
+            array.add(i);
+        }
+        List<Integer> array2 = new ArrayList<>();
+        for (int i = 0; i<22; i++) {
+            array2.add(i+100);
+        }
+        Collections.copy(array, array2);
+
+        for(int i=0; i<array.size(); i++) {
+            System.out.println(array.get(i));
         }
     }
 
     public static void testSort() {
+        System.out.println("----Test sort----");
         List<String> array = new DIYarrayList<>();
-        array.add("First");
-        array.add("Second");
-
-        for(int i=0; i<array.size(); i++) {
-            System.out.println(array.get(i));
-        }
+        Collections.addAll(array,"First", "Second", "1", "A", "s", "asw", "23", "delete", "one");
 
         Collections.sort(array, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                return 0;
+                return o1.compareToIgnoreCase(o2);
             }
         });
 
         for(int i=0; i<array.size(); i++) {
             System.out.println(array.get(i));
         }
-//        simpsons.stream().map(s -> s.name).forEach(System.out::print);
-        array.stream().forEach(System.out::println);
     }
 }
